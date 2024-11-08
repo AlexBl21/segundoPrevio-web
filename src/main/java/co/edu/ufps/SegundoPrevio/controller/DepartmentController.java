02b6bea219c5717f7b36a2459dd61ebd6cc50b3f
AlexBl21 <hblancorolon@gmail.com>
2024-11-08 12:13:41 -0500

    servicio de departamento por id

--- src/main/java/co/edu/ufps/SegundoPrevio/controller/DepartmentController.java
+++ src/main/java/co/edu/ufps/SegundoPrevio/controller/DepartmentController.java
@@ -1,5 +1,25 @@
 package co.edu.ufps.SegundoPrevio.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.ufps.SegundoPrevio.entities.Department;
+import co.edu.ufps.SegundoPrevio.services.DepartmentService;
+
+@RestController
+@RequestMapping("/department")
 public class DepartmentController {
+	
+	@Autowired
+	private DepartmentService departmentService;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Department> get(@PathVariable Integer id) {
+		return ResponseEntity.ok(departmentService.get(id));
+	}
 
 }
--- src/main/java/co/edu/ufps/SegundoPrevio/services/DepartmentService.java
+++ src/main/java/co/edu/ufps/SegundoPrevio/services/DepartmentService.java
@@ -1,5 +1,24 @@
 package co.edu.ufps.SegundoPrevio.services;
 
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import co.edu.ufps.SegundoPrevio.entities.Department;
+import co.edu.ufps.SegundoPrevio.repositories.DepartmentRepository;
+
+@Service
 public class DepartmentService {
 	
+	private DepartmentRepository departmentRepository;
+	
+	public Department get(Integer id) {
+		
+		
+		Optional<Department> DepartmentOpt = departmentRepository.findById(id);
+		if (DepartmentOpt.isPresent()) {
+			return DepartmentOpt.get();
+		}
+		return null;
+	}
 }
